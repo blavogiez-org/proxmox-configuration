5555c6136105877c4178afecc8ea0355bdebd58a
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-05-02 12:05:44 +0200

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Todo
+
+## Terraform
+
+- Qemu config d'une img template
+- Template terraform d'utilisation 
+
---
+++ terraform/debian/setup-cloud-template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash (to run as root)
+
+IMAGEURL=https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2
+
+wget $IMAGEURL
+qm create 9000 --name debian-13-template --memory 1024 --cores 1 --net0 virtio,bridge=vmbr0
+qm importdisk 9000 debian-13-genericcloud-amd64.qcow2 local-lvm
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
+qm set 9000 --boot c --bootdisk scsi0
+qm set 9000 --ide2 local-lvm:cloudinit
+qm set 9000 --serial0 socket --vga serial0
+qm set 9000 --agent enabled=1
+qm template 9000
