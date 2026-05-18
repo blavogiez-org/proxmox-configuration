7eb05d810ab43c2fc0dbb21d31c11a353db27508
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-05-18 23:28:35 +0200

    feat(backup): Backup remote sur un S3 Provider OK (vzdump, rclone en cronjob)

---
+++ backups/cron-rclone.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# m h  dom mon dow   command
+# 0 1  */1 *   *     bash /var/lib/vz/cron-rclone.sh
+
+set -euo pipefail
+
+S3_PROVIDER=scaleway
+BUCKET_NAME=proxmox-backups
+
+RETENT=true
+RETENTION=3d
+DUMPDIR=/var/lib/vz/dump
+
+vzdump --all --dumpdir=$DUMPDIR --mode snapshot --compress zstd
+
+
+rclone copy $DUMPDIR $S3_PROVIDER:$BUCKET_NAME/homelab/vzdump \
+  --progress \
+  --log-level INFO
+
+if [[ $RETENT = true ]]
+then 
+  rclone delete $S3_PROVIDER:$BUCKET_NAME/homelab/vzdump \
+    --min-age $RETENTION \
+    --log-file=/var/log/proxmox-scaleway-retention.log \
+    --log-level=INFO
+fi
+
+
+# vu qu'on est en set -e si on arrive là c'est que le s3 provider a bien reçu le backup 
+# dans mon cas je préfère clean en local (manque d'espace) 
+# petite securite vu que c'est une variable
+if [[ "$DUMPDIR" = "/var/lib/vz/dump" ]]; then
+  rm -rf "$DUMPDIR"/*
+fi
