ebc14dc474c74c97b4d7dc0e4884e6cda8b2535b
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-06-25 19:23:28 +0200

    feat(network, vpn, coredns): Implémentation de CoreDNS (+ playbook) pour avoir des noms de domaines exclusifs en privé, comme vault par exemple. Le VPN utilise désormais ce DNS

---
+++ ansible/scripts/deploy-coredns.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$script_dir/.."
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventories/inventory.yml playbooks/deploy-coredns.yml
