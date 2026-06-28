1e908b6bca067a6c37dda8c5856e41a8041cec9a
Jonas0o0 <jonas.facon@proton.me>
2026-06-28 16:54:25 +0200

    fix(init): correction appelle des scripts

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Interrompt le script au moindre échec d'une commande
 set -e
 
 echo "==========================================================="
@@ -8,43 +9,49 @@ echo "==========================================================="
 
 REPO_URL="https://github.com/jobacogiez-org/proxmox-gitops.git"
 REPO_DIR="proxmox-gitops"
+BRANCH="26-remplissage-assisté-des-secrets-du-vault-openbao"
 
+# 1. Vérification de Git sur la machine hôte
 if ! command -v git >/dev/null 2>&1; then
     echo "[ERROR] Git n'est pas installé sur cette machine. Installation requise."
     exit 1
 fi
 
+# 2. Clonage ou mise à jour du dépôt
 echo -e "\n[ÉTAPE 0/5] Récupération du dépôt Git..."
 if [ -d "$REPO_DIR" ]; then
     echo "[INFO] Le dossier '$REPO_DIR' existe déjà. Mise à jour (git pull)..."
     cd "$REPO_DIR"
-    git pull origin main
+    git checkout "$BRANCH"
+    git pull origin "$BRANCH"
 else
-    echo "[INFO] Clonage depuis $REPO_URL..."
-    git clone "$REPO_URL"
+    echo "[INFO] Clonage de la branche $BRANCH depuis $REPO_URL..."
+    git clone -b "$BRANCH" "$REPO_URL"
     cd "$REPO_DIR"
 fi
 
+# 3. Configuration des droits (en pointant vers le dossier scripts/)
 echo "[INFO] Configuration des permissions d'exécution..."
-chmod +x CheckDependences.sh CreateTfvars.sh LunchTerraform.sh CreateBaoSecrets.sh
+chmod +x scripts/CheckDependences.sh scripts/CreateTfvars.sh scripts/LunchTerraform.sh scripts/CreateBaoSecrets.sh
 
+# 4. Lancement de la séquence
 echo -e "\n[ÉTAPE 1/5] Vérification des dépendances..."
-./CheckDependences.sh
+./scripts/CheckDependences.sh
 
 echo -e "\n[ÉTAPE 2/5] Création de la configuration Proxmox (tfvars)..."
-./CreateTfvars.sh < /dev/tty
+./scripts/CreateTfvars.sh < /dev/tty
 
 TFVARS_PATH="terraform/environments/production/terraform.tfvars"
 
 echo -e "\n[ÉTAPE 3/5] Déploiement de la couche 'bootstrap' (Terraform)..."
-./LunchTerraform.sh "$TFVARS_PATH" "bootstrap"
+./scripts/LunchTerraform.sh "$TFVARS_PATH" "bootstrap"
 
 echo -e "\n[ÉTAPE 4/5] Injection des secrets dans OpenBao..."
 read -p "Saisissez l'IP physique de votre Proxmox pour configurer le routage vers OpenBao (ex: 192.168.1.100) : " PROXMOX_IP < /dev/tty
-./CreateBaoSecrets.sh "$PROXMOX_IP" < /dev/tty
+./scripts/CreateBaoSecrets.sh "$PROXMOX_IP" < /dev/tty
 
 echo -e "\n[ÉTAPE 5/5] Déploiement de la couche 'core' (Terraform)..."
-./LunchTerraform.sh "$TFVARS_PATH" "core"
+./scripts/LunchTerraform.sh "$TFVARS_PATH" "core"
 
 echo -e "\n==========================================================="
 echo "[SUCCESS] L'initialisation de l'infrastructure est terminée."
