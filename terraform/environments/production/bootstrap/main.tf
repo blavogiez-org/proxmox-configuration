b5d80c346bdd5212f6794e54d71dadc71db6bc7f
Jonas0o0 <jonas.facon@proton.me>
2026-06-29 19:20:23 +0200

    fix(init): emplacement du template des vm

--- terraform/environments/production/bootstrap/main.tf
+++ terraform/environments/production/bootstrap/main.tf
@@ -21,6 +21,67 @@ module "pubvnet1" {
   subnet_gateway = "172.16.10.1"
 }
 
+resource "proxmox_download_file" "debian13" {
+  node_name    = var.node_name
+  datastore_id = "local"
+  content_type = "import"
+
+  # l'url n'est pas forcément stable, les qcow2 sont un peu rares pour les mirrors
+  url       = "https://laotzu.ftp.acc.umu.se/images/cloud/trixie/latest/debian-13-generic-amd64.qcow2"
+  file_name = "debian-13-genericcloud-amd64.qcow2"
+}
+
+resource "proxmox_virtual_environment_vm" "debian13" {
+  name      = "debian13-template"
+  node_name = var.node_name
+  vm_id     = 9000
+
+  started = true
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local"
+    file_id      = proxmox_download_file.debian13.id
+    interface    = "scsi0"
+    discard      = "on"
+  }
+
+  initialization {
+    datastore_id      = "local"
+    user_data_file_id = proxmox_virtual_environment_file.cloud_init.id
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  serial_device {}
+}
+
+resource "proxmox_virtual_environment_file" "cloud_init" {
+  node_name    = var.node_name
+  datastore_id = "local"
+  content_type = "snippets"
+
+  source_file {
+    path      = "${path.root}/../../../../services/template-vm/cloud-init.yml"
+    file_name = "cloud-init.yml"
+  }
+}
+
 # vm dédiée vault
 # https://github.com/openbao/openbao
 # https://hub.docker.com/r/openbao/openbao
--- terraform/environments/production/vm-template/README.md
+++
@@ -1,4 +0,0 @@
-création du Template pour les VM (ici Debian)
-
-cela va accélerer les installations des futures instances puisqu'on part d'une image extraite et on la modifie, plutôt que de créer à chaque fois
-il s'agit du standard recommandé pour faire du Terraform dans Proxmox
