fec438f095f4592b537683da03c208742a9118be
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-07-20 21:45:51 +0200

    feat(perso): terraform pour lab k8s pour tester un projet

--- terraform/environments/production/core/additions_fork.tf
+++ terraform/environments/production/core/additions_fork.tf
@@ -68,3 +68,72 @@ module "ck-x" {
     ssh_public_key   = trimspace(file(pathexpand(var.ssh_public_key_path)))
   })
 }
+
+module "k8s-arbitrary-control-plane-1" {
+  source = "../../../modules/vm"
+  name                = "k8s-arbitrary-control-plane-1"
+  username            = "admin"
+  node_name           = "pve1"
+  vm_id               = 261
+  vm_template_id      = 9000
+  vm_ip               = "172.16.10.61"
+  network_gateway     = "172.16.10.1"
+  ssh_public_key_path = var.ssh_public_key_path
+  target_datastore_id = var.storage
+
+  cpu       = 4
+  memory    = 8192
+  disk_size = 20
+
+  bridge = "pubvnet1"
+  user_data_raw = templatefile("${path.root}/../../../../services/base-vm/cloud-init.yml", {
+    hostname         = "k8s-arbitrary-control-plane-1"
+    ssh_public_key   = trimspace(file(pathexpand(var.ssh_public_key_path)))
+  })
+}
+
+module "k8s-arbitrary-worker-1" {
+  source = "../../../modules/vm"
+  name                = "k8s-arbitrary-worker-1"
+  username            = "admin"
+  node_name           = "pve1"
+  vm_id               = 262
+  vm_template_id      = 9000
+  vm_ip               = "172.16.10.62"
+  network_gateway     = "172.16.10.1"
+  ssh_public_key_path = var.ssh_public_key_path
+  target_datastore_id = var.storage
+
+  cpu       = 4
+  memory    = 8192
+  disk_size = 20
+
+  bridge = "pubvnet1"
+  user_data_raw = templatefile("${path.root}/../../../../services/base-vm/cloud-init.yml", {
+    hostname         = "k8s-arbitrary-worker-1"
+    ssh_public_key   = trimspace(file(pathexpand(var.ssh_public_key_path)))
+  })
+}
+
+module "k8s-arbitrary-worker-2" {
+  source = "../../../modules/vm"
+  name                = "k8s-arbitrary-worker-2"
+  username            = "admin"
+  node_name           = "pve1"
+  vm_id               = 263
+  vm_template_id      = 9000
+  vm_ip               = "172.16.10.63"
+  network_gateway     = "172.16.10.1"
+  ssh_public_key_path = var.ssh_public_key_path
+  target_datastore_id = var.storage
+
+  cpu       = 4
+  memory    = 8192
+  disk_size = 20
+
+  bridge = "pubvnet1"
+  user_data_raw = templatefile("${path.root}/../../../../services/base-vm/cloud-init.yml", {
+    hostname         = "k8s-arbitrary-worker-2"
+    ssh_public_key   = trimspace(file(pathexpand(var.ssh_public_key_path)))
+  })
+}
