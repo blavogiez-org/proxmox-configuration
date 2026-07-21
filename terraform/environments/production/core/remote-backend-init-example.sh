3be32fedc34cc18a5b647198a002153db0b57523
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-07-21 13:35:42 +0200

    feat(terraform, remote backend/state): Backend http + loading secrets

---
+++ terraform/environments/production/core/remote-backend-init-example.sh
@@ -0,0 +1,3 @@
+# pour configurer les credentials d'accès au remote backend
+export TF_HTTP_USERNAME="exemple"
+export TF_HTTP_PASSWORD="exemple"
--- terraform/environments/production/core/versions.tf
+++ terraform/environments/production/core/versions.tf
@@ -7,4 +7,15 @@ terraform {
       version = "= 0.107.0"
     }
   }
+
+  backend "http" {
+    # les variables terraform ne sont pas possibles ici (pour pas mettre en clair), il faut export en env
+    # faire remote-backend-init.sh avant
+    address = "https://terraform-backend.priv.blavogiez.fr/client/maintainers/proxmox-gitops/pve1/state"
+    lock_address = "https://terraform-backend.priv.blavogiez.fr/client/maintainers/proxmox-gitops/pve1/lock"
+    unlock_address = "https://terraform-backend.priv.blavogiez.fr/client/maintainers/proxmox-gitops/pve1/unlock"
+    lock_method = "POST"
+    unlock_method = "POST"
+  }
+
 }
