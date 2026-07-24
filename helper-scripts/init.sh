7ad09c9400216e80887b166ba06c2e8450d1d081
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-07-24 20:32:46 +0200

    feat(ansible, makefile): make pour deploy alloy / bootstrap lxc

--- helper-scripts/init.sh
+++ helper-scripts/init.sh
@@ -26,28 +26,28 @@ else
 fi
 
 echo "[INFO] Configuration des permissions d'exécution..."
-chmod +x scripts/check_dependencies.sh scripts/create_tfvars_credentials.sh scripts/launch_terraform_by_layer.sh scripts/create_main_vault_secrets.sh scripts/create_repo_settings.sh scripts/initialize_vault.sh
+chmod +x helper-scripts/check_dependencies.sh helper-scripts/create_tfvars_credentials.sh helper-scripts/launch_terraform_by_layer.sh helper-scripts/create_main_vault_secrets.sh helper-scripts/create_repo_settings.sh helper-scripts/initialize_vault.sh
 
 echo -e "\n[ÉTAPE 1/6] Vérification des dépendances (outils CLI)..."
-./scripts/check_dependencies.sh
+./helper-scripts/check_dependencies.sh
 
 echo -e "\n[ÉTAPE 2/6] Création de la configuration Proxmox (tfvars)..."
-./scripts/create_tfvars_credentials.sh < /dev/tty
+./helper-scripts/create_tfvars_credentials.sh < /dev/tty
 
 TFVARS_PATH="terraform/environments/production/terraform.tfvars"
 
 echo -e "\n[ÉTAPE 3/6] Déploiement de la couche 'bootstrap' (Terraform)..."
-./scripts/launch_terraform_by_layer.sh "$TFVARS_PATH" "bootstrap"
+./helper-scripts/launch_terraform_by_layer.sh "$TFVARS_PATH" "bootstrap"
 
 echo -e "\n[ÉTAPE 4/6] Injection des secrets dans OpenBao..."
 read -r -p "Saisissez l'IP physique de votre Proxmox pour configurer le routage vers OpenBao (ex: 192.168.1.100) : " PROXMOX_IP < /dev/tty
-source ./scripts/create_main_vault_secrets.sh "$PROXMOX_IP" < /dev/tty
+source ./helper-scripts/create_main_vault_secrets.sh "$PROXMOX_IP" < /dev/tty
 
 echo -e "\n[ÉTAPE 5/6] Génération de la configuration globale (settings.yml)..."
-./scripts/create_repo_settings.sh < /dev/tty
+./helper-scripts/create_repo_settings.sh < /dev/tty
 
 echo -e "\n[ÉTAPE 6/6] Déploiement de la couche 'core' (Terraform)..."
-./scripts/launch_terraform_by_layer.sh "$TFVARS_PATH" "core"
+./helper-scripts/launch_terraform_by_layer.sh "$TFVARS_PATH" "core"
 
 echo -e "\n==========================================================="
 echo "[SUCCESS] L'initialisation de l'infrastructure est terminée."
