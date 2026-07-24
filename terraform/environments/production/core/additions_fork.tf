a91bd8fcae64eefec066045628341f0c2e197f7d
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2026-07-24 20:30:50 +0200

    chore(tf): delete de nextcloud, pas encore besoin et prend du disque

--- terraform/environments/production/core/additions_fork.tf
+++ terraform/environments/production/core/additions_fork.tf
@@ -1,28 +1,3 @@
-
-module "nextcloud" {
-  source = "../../../modules/vm"
-  name                = "nextcloud"
-  username            = "admin"
-  node_name           = "pve1"
-  vm_id               = 213
-  vm_template_id      = 9000
-  vm_ip               = "172.16.10.13"
-  network_gateway     = "172.16.10.1"
-  ssh_public_key_path = var.ssh_public_key_path
-  target_datastore_id = var.storage
-
-  cpu       = 1
-  memory    = 2048
-  disk_size = 500
-
-  bridge = "pubvnet1"
-  user_data_raw = templatefile("${path.root}/../../../../services/base-vm/cloud-init.yml", {
-    hostname         = "nextcloud"
-    ssh_public_key   = trimspace(file(pathexpand(var.ssh_public_key_path)))
-  })
-}
-
-
 module "vaultwarden" {
   source = "../../../modules/vm"
   name                = "vaultwarden"
